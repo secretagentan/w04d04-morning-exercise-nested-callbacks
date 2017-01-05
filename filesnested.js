aeeaa9c3b4708658f408c251bdbc17563740e49f
Boshika Tara <boshikatara@gmail.com>
2017-01-05 07:33:32 -0800

    Adds callback hell

---
+++ filesnested.js
@@ -0,0 +1,17 @@
+/*
+   Goal of this exercise is to learn nested callbacks.
+*/
+
+//require fs
+
+
+//read file1
+    //read file2
+        //read file3
+          //join file1, fil2 and file3 contents
+          //write to file4
+              // console.log('Concatenated file1.txt, file2.txt, file3.txt into file4.txt.');
+              //read file4
+                //write file5
+                  // console.log('File4 content was copied over to file5')
+
