14f90895e1922784aa045670e295c9bc84aa885f
an nguyen <hello.an.ng@gmail.com>
2017-01-10 00:06:51 -0800

    saves solution

--- filesnested.js
+++ filesnested.js
@@ -4,7 +4,6 @@
 
 //require fs
 
-
 //read file1
     //read file2
         //read file3
@@ -14,4 +13,28 @@
               //read file4
                 //write file5
                   // console.log('File4 content was copied over to file5')
+
+var fs = require('fs');
+fs.readFile('./file1.txt', 'utf-8', function(err, data1) {
+    if(err) throw err;
+    fs.readFile('./file2.txt', 'utf-8', function(err, data2){
+      if (err) throw err;
+      fs.readFile('./file3.txt', 'utf-8', function(err, data3){
+        if(err) throw err;
+        var allData = data1 + '\n' + data2 + '\n' + data3;
+        // console.log(allData);
+        fs.writeFile('file4.txt', allData, function(err){
+          if (err) throw err;
+          // console.log('file4 written');
+          fs.readFile('./file4.txt', 'utf-8', function(err, data4){
+            if(err) throw err;
+            fs.writeFile('file5.txt', data4, function(err){
+            if (err) throw err;
+            // console.log("file 4 copied");
+            })
+          })
+        })
+      })
+    })
+})
 
---
+++ practice.js
@@ -0,0 +1,11 @@
+// var str = "name,age\nharry,12"
+// var keysAndVals = str.split('\n');
+// console.log(keysAndVals);
+// var keys = keysAndVals[0].split(',');
+// var vals = keysAndVals[1].split(',');
+// var obj = {};
+// for (var i = 0; i < keys.length; i++) {
+//   var key = keys[i];
+//   var val = vals[i];
+//   obj[key] = val;
+// }
